dfe019191eca2a0058e7b1b434c3108ddbd03de4
HongyunQiu <qiuhy02@gmail.com>
2023-07-06 18:49:56 +0800

    mod

--- train.py
+++ train.py
@@ -91,6 +91,8 @@ if ddp:
     seed_offset = ddp_rank # each process gets a different seed
     # world_size number of processes will be training simultaneously, so we can scale
     # down the desired gradient accumulation iterations per process proportionally
+    print(gradient_accumulation_steps)
+    print(ddp_world_size)
     assert gradient_accumulation_steps % ddp_world_size == 0
     gradient_accumulation_steps //= ddp_world_size
 else:
@@ -184,6 +186,8 @@ elif init_from.startswith('gpt2'):
     for k in ['n_layer', 'n_head', 'n_embd', 'block_size', 'bias', 'vocab_size']:
         model_args[k] = getattr(model.config, k)
 # crop down the model block size if desired, using model surgery
+print ("block size" ,block_size)
+print("model.config.block_size",model.config.block_size)
 if block_size < model.config.block_size:
     model.crop_block_size(block_size)
     model_args['block_size'] = block_size # so that the checkpoint will have the right value
